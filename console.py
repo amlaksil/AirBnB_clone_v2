fb7f6e75791fb43d95cfc5e92c3627ea515ac05a
amlaksil <silamlakdesye@gmail.com>
2023-06-21 16:49:39 +0300

    Update do_create method by allow object creation with given parameters

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -73,8 +74,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] == '}'\
+                            and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -115,16 +116,36 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        cmds = args.replace(' ', ' "').replace('=', '"=')
+        cmds = cmds.split(' ', 1)
+        cls = cmds[0]
+        if len(cmds) == 0:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif cmds[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        # convert values to dictionary object
+        if (len(cmds) > 1):
+            values = '{' + cmds[1] + '}'
+            values = values.replace(' ', ', ').replace('=', ': ')
+            dict_t = json.loads(values)
+            dict_tt = {}
+            # If <value> is string: replace all underscores by spaces
+            for k, v in dict_t.items():
+                if type(dict_t[k]) is str:
+                    dict_tt[k] = dict_t[k].replace('_', ' ')
+                else:
+                    dict_tt[k] = v
+            new_instance = HBNBCommand.classes[cls]()
+            key = f"{cmds[0]}.{new_instance.id}"
+            for k, v in dict_tt.items():
+                setattr(storage.all()[key], k, v)
+                storage.save()
+        else:
+            new_instance = HBNBCommand.classes[cls]()
+            storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -264,7 +285,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         # first determine if kwargs or args
-        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
+        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) == dict:
             kwargs = eval(args[2])
             args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
             for k, v in kwargs.items():
@@ -272,7 +293,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +301,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +340,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
