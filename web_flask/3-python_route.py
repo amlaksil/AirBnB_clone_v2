08e1b146886467372d0e38124162cb2c5c4a5421
amlaksil <silamlakdesye@gmail.com>
2023-07-23 09:17:02 +0300

    Add another route called python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains a code that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a string """
+    return f'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string """
+    return f'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Returns a string """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python')
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_str(text="is cool"):
+    """Returns a string """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0', debug=True)
